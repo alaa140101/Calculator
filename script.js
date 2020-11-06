e57f5f896f9e679ddc8211b85acda75e2d678a79
alaa140101 <alaa140011@gmail.com>
2020-11-06 18:23:36 +0300

    my try for decimal and clear

--- script.js
+++ script.js
@@ -2,20 +2,38 @@ const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 
+let decimalAdded = false;
 
 function sendNumberValue(number){
-  // If current display value is 0, replace it, if not add number
-  const displayValue = calculatorDisplay.textContent;
-  calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+  // If current display value is 0, replace it, if not add number  
+  const displayValue = calculatorDisplay.textContent;    
+  // if (number !== '.') {
+  //   calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+  // }else if (number === '.' && !decimalAdded) {
+  //   calculatorDisplay.textContent = displayValue === '0' ? `0${number}` : displayValue + number;
+  //   decimalAdded = true;
+  // }
+  // console.log(number);
+  if (number !== '.' || !decimalAdded) {
+    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+    if (number === '.'){
+      decimalAdded = true;
+    }
+  }
 }
 
 // Add Event Listeners for numbers, operators, decimal buttons
   inputBtns.forEach( inputBtn => {
     if (inputBtn.classList.length === 0) {
-     return inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value))
+      inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
     }else if (inputBtn.classList.contains('operator')) {
-      return inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value))
+      inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
     }else if (inputBtn.classList.contains('decimal')) {
-      inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value))
+      inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
     }
+  });
+
+  clearBtn.addEventListener('click', () => {
+    calculatorDisplay.textContent = '0'
+    decimalAdded = false;
   });
